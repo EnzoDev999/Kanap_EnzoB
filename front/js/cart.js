ec2cd2900c0f93e726c23fd3bb54929bea331eed
Innocxnt <Innocxnt999@gmail.com>
2022-12-01 19:58:21 +0100

    création de la fonction d'affichage qui va permettre d'afficher les produitss du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,9 +9,13 @@ const page = document.location.href;
 si c'est confirmé alors on est bien dans la page panier */
 if (page.match("cart")) {
     fetch("http://localhost:3000/api/products")
+        // Fetch retourne une promese que .json va retourner en json
         .then((res) => res.json())
+        // Je nomme la promese en "objectProducts"
         .then((objectProducts) => {
+            // Vérification que tout est bien affiché
             console.log(objectProducts);
+             // J'appel ensuite ma fonction
             displayCart(objectProducts);
         })
         .catch((err) => {
@@ -20,4 +24,74 @@ if (page.match("cart")) {
           });
 } else {
     console.log("page confirmation affichée")
+}
+
+//---------------------------------------------------------------------------------------------------------
+// Création de la fonction displayCart qui va déterminer les conditions d'affichage des produits du panier
+//---------------------------------------------------------------------------------------------------------
+
+function displayCart(products) {
+    // on récupère le panier (qui est en string car provient du localStorage) qu'on convertit en object JS
+    let cart = JSON.parse(localStorage.getItem("storageCart"));
+    // S'il y a un panier avec une taille differante de 0 (donc supérieure à 0)
+    if (cart && cart.length != 0) {
+     // zone de correspondance clef/valeur de l'api et du panier grâce à l'id produit choisit dans le localStorage
+     for (let choice of cart) {
+        console.log(choice);
+        products.forEach(element => {
+            if (choice._id === element._id) {
+                    choice.name = element.name;
+                    choice.price = element.price;
+                    choice.image = element.imageUrl;
+                    choice.description = element.description;
+                    choice.alt = element.altTxt;
+            }
+        });
+     }
+     // On appel la fonction display() qui va utiliser les données de cart(définies juste au dessus)
+    display(cart);
+    } else {
+        // Si le panier se retrouve vide, on créer un h1 qui va servire d'inforamation
+        document.querySelector("#totalQuantity").innerHTML = "0";
+        document.querySelector("#totalPrice").innerHTML = "0";
+        document.querySelector("h1").innerHTML = 
+        "Vous n'avez pas d'article dans votre panier.";
+    }
+    // reste à l'écoute grâce aux fonctions suivantes pour modifier l'affichage au besoin
+    changeQuantity();
+    deleteProduct();
+}
+
+//--------------------------------------------------------------
+// Fonction d'affichage d'un panier (tableau)
+//--------------------------------------------------------------
+
+// on créer la fonction display() avec un params qui va permettre l'appel de données à appliqué à cette fonction (donc utilisera les données de "cart")
+function display(indexé) {
+    // on déclare et on pointe la zone d'affichage
+    let zoneCart = document.querySelector("#cart_items");
+    // on créer l'affichage des produits du panier via un map et on introduit les dataset(défini plus tôt) dans le code
+    zoneCart.innerHTML += indexé.map((choice) => 
+    `<article class="cart__item" data-id="${choice._id}" data-quantity="${choix.color}" data-quantity="${choice.quantity}" data-price="${choice.price}"> 
+    <div class="cart__item__img">
+      <img src="${choice.image}" alt="${choice.alt}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__titlePrice">
+        <h2>${choice.name}</h2>
+        <span>couleur : ${choice.color}</span>
+        <p data-prix="${choice.price}">${choice.price} €</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${choice.quantity}">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem" data-id="${choice._id}" data-couleur="${choice.color}">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>`
+    ) 
 }
